6346f9422e0b1c51fb6a17a248e426fe2aa43236
Martynas-J <Aurimhas@gmail.com>
2025-03-01 15:21:23 +0200

    Atnaujinau kodą

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://barcode-scanner-omega.vercel.app";
+export const API_URL = "https://k-sarasas.vercel.app";
 //export const API_URL = "http://localhost:3000";
 export const TABLEHEADER = ["Nr.", "Prekė", "Likutis"];
 export const STATHEADER = ["Vardas", "Prekė", "Kiekis", "Veiksmas", "Data"];
